2ad2d4ecd7aa50ff0863aaf970cc6ce1acdbcd46
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-19 10:06:10 +0530

    create basic layout of footer

--- wp-content/themes/demo-company/footer.php
+++ wp-content/themes/demo-company/footer.php
@@ -14,13 +14,23 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="container">
+			<div class="footer-main">
+				<div class="container">
+					<div class="footer-top">
+						<div class="footer-col-1"></div>
+						<div class="footer-col-2"></div>
+						<div class="footer-col-3"></div>
+						<div class="footer-col-4"></div>
+					</div>
+				</div>
+			</div>
 			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo-company' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'demo-company' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo-company' ), 'demo-company', '<a href="https://automattic.com/" rel="designer">Rashmi M</a>' ); ?>
-			</div><!-- .site-info -->
-		</div>
+				<div class="container">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo-company' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'demo-company' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo-company' ), 'demo-company', '<a href="https://automattic.com/" rel="designer">Rashmi M</a>' ); ?>
+				</div><!-- .site-info -->
+			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
